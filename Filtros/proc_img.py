3cd469f013b8a301bd9950c0d11d27ed4fea4116
Lygia Marina <lygia.marina@gmail.com>
2014-04-02 15:55:09 -0300

    Adicionando filtros já feitos anteriormente

---
+++ Filtros/proc_img.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+from PIL import Image
+from filtros import *
+import sys
+
+class MyImg:
+	def __init__(self, imgDir):
+		self.imgDir = imgDir
+
+		aux = imgDir.split('/')		
+		self.outImgName = aux[-1]
+
+		self.img = Image.open(self.imgDir).convert("L")
+
+		self.width = self.img.size[0]
+		self.height = self.img.size[1]
+
+	def getSize(self):
+		return self.img.size
+
+	def getMode(self):
+		return self.img.mode
+
+	def setOutName(self, imgName):
+		self.outImgName = imgName
+
+	def show(self):
+		self.img.show()
+
+	def save(self):
+		self.img.save(self.outImgName)
+
+	def buildPixelsVector(self, window, pixels, x, y):
+		neighbors = window/2
+		pixelsVector = []
+		pixelsVector.append(pixels[x,y])
+		for i in range(window-1):
+			for j in range(window-1):
+				if (neighbors+i != neighbors+j):
+					try:
+						pixelsVector.append(pixels[x-neighbors+i,y-neighbors+i])
+					except IndexError:
+						pass
+		return pixelsVector
+
+
+	def applySimpleFilter(self, func, extraArgs):
+		self.setOutName(func.__name__  +'--'+ self.outImgName)
+	
+		self.pixels = self.img.load()
+		for x in range(self.width):
+			for y in range(self.height):
+				pixel = self.pixels[x,y]
+				self.pixels[x,y] = func(pixel, *extraArgs)
+
+	def applyComplexFilter(self, func, extraArgs, window=3):
+		self.setOutName(func.__name__ +'--'+ self.outImgName)
+		neighbors = window/2
+		self.pixels = self.img.load()
+		self.copyImg = self.img.copy()
+		self.copyPixels = self.copyImg.load()
+		for x in range(1,self.width-1):
+			for y in range(1,self.height-1):
+
+				"""
+					| i=2 | i=5 | i=4 |
+					| i=1 |  0  | i=3 |
+					| i=6 | i=7 | i=8 |
+				"""
+
+				if (window == 3):
+					pixelsVector = [self.pixels[x,y],
+							self.pixels[x-1,y], self.pixels[x-1,y-1],
+							self.pixels[x+1,y], self.pixels[x+1,y-1],
+							self.pixels[x,y-1], self.pixels[x-1,y+1],
+							self.pixels[x,y+1], self.pixels[x+1, y+1]]
+				else:
+					pixelsVector = self.buildPixelsVector(window, self.pixels, x, y)
+				
+				self.copyPixels[x,y] = func(pixelsVector, *extraArgs)
+		self.img = self.copyImg.copy()
+
+
---
+++ Filtros/transformadas.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+class Transformadas:
+	def fourier2D
