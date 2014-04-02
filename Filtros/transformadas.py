9124f59155b6b7c36465b635f86cc6a1637d7ff0
niltonduarte <niltonduarte@vm-0.niltonduarte.koding.kd.io>
2014-04-02 19:48:17 +0000

    adicionado FFT e makefile

--- Filtros/transformadas.py
+++ Filtros/transformadas.py
@@ -1,4 +1,16 @@
-import numpy as np
+from cmath import exp, pi
 
 class Transformadas:
-	def fourier2D
+    
+	def fft(self, x):
+        N = len(x)
+        if N <= 1: return x
+        even = fft(x[0::2])
+        odd =  fft(x[1::2])
+        return [even[k] + exp(-2j*pi*k/N)*odd[k] for k in xrange(N/2)] + \
+               [even[k] - exp(-2j*pi*k/N)*odd[k] for k in xrange(N/2)]
+               
+    def imgFFT(self, img):
+        #passar cada linha /coluna da imagem para o fft, e calcular a magnitude no ponto x,y dado pelo fft da linha x e coluna y
+
+                
