af5597946c24dfb99a09783c7f5c39ededfcbc0c
Nilton Guedes Duarte <nilton.gduarte@gmail.com>
2014-05-23 00:27:33 -0300

    lyly me dando trabalho

--- Harris/source/eyeTracking.py
+++ Harris/source/eyeTracking.py
@@ -4,22 +4,27 @@ class eyeTracking:
 
 
 	def __init__(self):
-		self.eyeCoord = [0,0]
-		self.pEyeCoord = [0,0]
+		self.rightEyeCoord = [0,0]
+		self.pRightEyeCoord = [0,0]
+		self.leftEyeCoord = [0,0]
+		self.pLeftEyeCoord = [0,0]
 	
-	def harrisGreatestValueCoord(self)
+	def harrisGreatestValueCoord(self, coord)
 		img = None;
-		harrisImg = harrisImg(eyeCoord[0], eyeCoord[1], img);
+		harrisImg = harrisImg(coord[0], coord[1], img);
 		maxHarris = 0;
 		harrisCoord = [0,0]
 		for i in range(len(harrisImg)):
 			for j in range(len(harrisImg[0])):
-				if(harrisImg > maxHarris)
+				if(harrisImg > maxHarris):
 					maxHarris = harrisImg
 					harrisCoord = [i,j]
 		globalCoord = [eyeCoord[0] + harrisCoord[0] - 25, eyeCoord[1] + harrisCoord[1] - 25]
 		return globalCoord	
 	
 	def eyeTracking():
-		pEyeCoord = eyeCoord
-		eyeCoord = harrisGreatestValueCoord()
+		pRightEyeCoord = eyeCoord
+		rightEyeCoord = harrisGreatestValueCoord(rightEyeCoord)
+		pLeftEyeCoord = leftEyeCoord
+		leftEyeCoord = harrisGreatestValueCoord(leftEyeCoord)
+		prevDistance = ((pRightEyeCoord[0] - pLeftEyeCoord[0])**2 + (pRightEyeCoord[1] - pLeftEyeCoord[1])**2)**0.5
