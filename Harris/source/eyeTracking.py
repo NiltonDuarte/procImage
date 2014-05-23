04cee7500575c3faaa371b5e0c283e594efc601d
Nilton Guedes Duarte <nilton.gduarte@gmail.com>
2014-05-23 00:45:21 -0300

    yay

--- Harris/source/eyeTracking.py
+++ Harris/source/eyeTracking.py
@@ -8,6 +8,8 @@ class eyeTracking:
 		self.pRightEyeCoord = [0,0]
 		self.leftEyeCoord = [0,0]
 		self.pLeftEyeCoord = [0,0]
+		self.pTheta = 0
+		self.theta = 0
 	
 	def harrisGreatestValueCoord(self, coord)
 		img = None;
@@ -19,12 +21,16 @@ class eyeTracking:
 				if(harrisImg > maxHarris):
 					maxHarris = harrisImg
 					harrisCoord = [i,j]
-		globalCoord = [eyeCoord[0] + harrisCoord[0] - 25, eyeCoord[1] + harrisCoord[1] - 25]
+		globalCoord = [coord[0] + harrisCoord[0] - 25, coord[1] + harrisCoord[1] - 25]
 		return globalCoord	
 	
 	def eyeTracking():
-		pRightEyeCoord = eyeCoord
-		rightEyeCoord = harrisGreatestValueCoord(rightEyeCoord)
-		pLeftEyeCoord = leftEyeCoord
-		leftEyeCoord = harrisGreatestValueCoord(leftEyeCoord)
-		prevDistance = ((pRightEyeCoord[0] - pLeftEyeCoord[0])**2 + (pRightEyeCoord[1] - pLeftEyeCoord[1])**2)**0.5
+		self.pRightEyeCoord = self.eyeCoord
+		self.rightEyeCoord = harrisGreatestValueCoord(self.rightEyeCoord)
+		self.pLeftEyeCoord = self.leftEyeCoord
+		self.leftEyeCoord = harrisGreatestValueCoord(self.leftEyeCoord)
+		prevDistance = ((self.pRightEyeCoord[0] - self.pLeftEyeCoord[0])**2 + (self.pRightEyeCoord[1] - self.pLeftEyeCoord[1])**2)**0.5
+		distance = ((self.rightEyeCoord[0] - self.leftEyeCoord[0])**2 + (self.rightEyeCoord[1] - self.leftEyeCoord[1])**2)**0.5
+		self.pTheta = self.theta
+		self.theta = math.acos(distance/prevDistance)
+		
